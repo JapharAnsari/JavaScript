b8f78763ae333281016a81a75102b8bf3e69bbcd
Japhar Ansari <102852654+JapharAnsari@users.noreply.github.com>
2025-11-24 06:47:07 +0000

    ConversionOfDataType

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,17 @@
+let score = 103
+
+console.log(typeof score)
+console.log(typeof (score))
+
+//Example
+
+let newscore = "103"        
+let value = Number(newscore)      // Type Casting
+console.log(typeof value)
+
+// Example
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
