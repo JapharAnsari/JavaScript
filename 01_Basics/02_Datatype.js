07f0c99f050ff012f90ef964ed43d6117054f3e5
Japhar Ansari <102852654+JapharAnsari@users.noreply.github.com>
2025-11-24 06:30:29 +0000

    Done

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,17 @@
+ // DataType
+
+ let name = "Japhar"    // string data ttype
+ let age = 18
+ let isLoggedIn = false
+
+ let State = null
+
+  //// and others datatype
+
+  // null = standalone value
+
+console.log(typeof "Japhar")
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)    // object
+
