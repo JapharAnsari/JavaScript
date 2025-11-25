efd3748f6386648636c0515a9f5ce3b02b022568
Japhar Ansari <102852654+JapharAnsari@users.noreply.github.com>
2025-11-25 04:32:28 +0000

    String

---
+++ 01_Basics/06_String.js
@@ -0,0 +1,60 @@
+  
+  // ************* String *************
+
+const name = "Japhar"
+const repoCount = 103
+
+console.log(name, repoCount, "Ansari");
+
+
+
+const gameName = new String('Japhar Ansari')
+
+console.log(gameName);
+console.log(gameName[3]);
+console.log(gameName. __proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('p'));
+
+
+/// More Example
+
+const newValue = new String("Padrauna")
+const newString = newValue.substring(1,4)
+console.log(newString);
+
+const anotherName = newValue.slice(0,3)
+console.log(anotherName);
+
+
+//   More
+
+const newString1  = "     Kushinagar     "
+console.log(newString1);
+
+const url = "http.newslap&japhar@.com"
+console.log(url.replace('japhar','pintu'));
+
+console.log(url.includes("japhar"))
+console.log(url.includes("pintu"));
+
+console.log(newString1.split('-'));
+console.log(gameName.split('-'));
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
