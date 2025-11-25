360ae49ca949682f9a2df936200307d8d3412a52
Japhar Ansari <102852654+JapharAnsari@users.noreply.github.com>
2025-11-25 05:19:49 +0000

    Date and Time

---
+++ 01_Basics/08_dateTime.js
@@ -0,0 +1,25 @@
+
+// ******************Date******************
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.getHours());
+// console.log(typeof myDate);
+
+
+// More Example
+
+let myCreateDate = new Date(2003,1,7)
+console.log(myCreateDate.toDateString());
+
+let myTime = Date.now()
+console.log(myTime);
+
+
+
+
+
+
