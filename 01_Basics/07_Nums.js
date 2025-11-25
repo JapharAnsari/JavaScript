52375b9e765b96a779e6066dbd509a83e0bf37fb
Japhar Ansari <102852654+JapharAnsari@users.noreply.github.com>
2025-11-25 05:03:33 +0000

    Nums and Math

---
+++ 01_Basics/07_Nums.js
@@ -0,0 +1,58 @@
+
+// ****************** Numbers *****************
+
+// const score = 500
+// console.log(score);
+
+// // More Example 
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2))
+
+
+// // More Example
+
+// const otherNumber = 123.5555
+// console.log(otherNumber.toExponential(2));
+
+// const pintu = 2000000
+// console.log(pintu.toLocaleString());
+
+
+
+
+// **************************Maths*****************************
+
+console.log(Math);
+console.log(Math.abs(-7))
+console.log(Math.abs(7));
+console.log(Math.round(4.8));
+console.log(Math.ceil(2.2));
+console.log(Math.floor(1.9));
+console.log(Math.min(2,4,7,4,9));
+console.log(Math.max(4,8,9,2));
+console.log(Math.random());
+
+// More Example of Math
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random() * (max-min+1)));
+
+
+
+
+
+
+
+
+
+
+
+
