27a7674156f1cb3f0288f8d48da3f8be24ad4c60
Japhar Ansari <102852654+JapharAnsari@users.noreply.github.com>
2025-12-01 03:02:56 +0000

    Iteration

---
+++ 05_Iteration/02_Two.js
@@ -0,0 +1,27 @@
+
+// While loop
+
+let index = 0
+while(index <=10){
+    console.log(`Value of index is ${index}`);
+    index = index+2
+    
+}
+
+//
+ let myArray = ["Japhar","Ansari","Padrauna","Kushinagar","Lucknow"]
+ let arr = 0
+ while(arr < myArray.length){
+    console.log(`Value of myArray is ${myArray[arr]}`);
+    arr = arr+1
+    
+ }
+
+
+ // Do While loop
+
+ let score = 1
+ do{
+    console.log(`Value is ${score}`);
+    score++ 
+ } while(score <=10)
